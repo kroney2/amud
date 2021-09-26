05cdbdcab54633adabd074d3bc31259970477d08
Caleb Barger <bargercj20@hotmail.com>
2021-09-26 13:34:48 -0700

    async msg logger

--- mud.c
+++ mud.c
@@ -16,7 +16,7 @@
 #include <poll.h>
 #include <pthread.h>
 
-//#define _DEBUG
+#define _DEBUG
 
 // server settings
 #define REMOTEHOST "localhost"
@@ -27,12 +27,13 @@
 
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 800
+#define LOG_SPEED 150
 // map dims
 #define FONT_SIZE 25
 #define MAP_WIDTH 30
-#define MAP_HEIGHT 20
-#define MIN_RM_SIZE 8
-#define MAX_RM_SIZE MAP_WIDTH - MIN_RM_SIZE
+#define MAP_HEIGHT 19
+#define MIN_RM_WIDTH 3
+#define MIN_RM_HEIGHT 5
 
 typedef enum _tiles_e
 {
@@ -42,31 +43,22 @@ typedef enum _tiles_e
     SPACE=' '-ASCII_OFFSET
 } tiles_e;
 
-typedef struct _bsp_node_t
+typedef struct _as_arg
 {
-    SDL_Rect r;
-    int connected;
-    struct _bsp_node_t* children[2];
-    struct _bsp_node_t* parent;
-} bsp_node_t;
+    SDL_Renderer* renderer;
+    SDL_Rect* logr;
+    int logr_startx;
+    int logr_starty;
+    SDL_Texture** texture_cache;
+    char* msg;
+} as_arg_t;
 
-typedef struct _bsp_queue_t
-{
-    bsp_node_t* data;
-    struct _bsp_queue_t* next;
-} bsp_queue_t;
-
-int make_rms(bsp_node_t* root, int axis);
+void* poll_serv(void* as_arg);
+void* tf_log_handler(void* as_arg);
 int get_conn();
-void* poll_serv(void* arg);
 
 int main()
 {
-    char buf[256]; // msg data;
-    strcpy(buf, "NOT CONNECTED TO SERV!");
-    pthread_t th;
-    pthread_create(&th, NULL, poll_serv, buf);
-
     SDL_Init(SDL_INIT_VIDEO);
     SDL_Window* window;
     window = SDL_CreateWindow(
@@ -118,7 +110,7 @@ int main()
         }
         else
         {
-            c.r = 255; c.g = 255; c.b = 255;
+            c.r = 0; c.g = 255; c.b = 0;
         }
         SDL_Surface* glyph_surf = TTF_RenderGlyph_Solid(font, ch, c);
         texture_cache[ch - ASCII_OFFSET] = SDL_CreateTextureFromSurface(renderer,
@@ -137,80 +129,42 @@ int main()
     logr.h = mapr.h;
 
     mapr.w = ((SCREEN_WIDTH / 4) * 3)/MAP_WIDTH;
-    mapr.h = SCREEN_HEIGHT/MAP_HEIGHT;
+    mapr.h = ((SCREEN_HEIGHT / 4) * 3)/MAP_HEIGHT;
 
-    logr.x = (mapr.w * MAP_WIDTH)+logr.w;
-    logr.y = 0;
+//    logr.x = (mapr.w * MAP_WIDTH)+logr.w;
+    logr.x = 0;
+    logr.y = (mapr.h * MAP_HEIGHT);
 
     int logr_startx = logr.x, logr_starty = logr.y;
 
-    bsp_node_t root;
-    root.parent = NULL;
-    root.r.w = MAP_WIDTH;
-    root.r.h = MAP_HEIGHT;
-    root.r.x = 0;
-    root.r.y = 0;
-
-    bsp_queue_t* q = (bsp_queue_t*)calloc(1, sizeof(bsp_queue_t));
-    q->data = &root;
-    q->next = NULL;
-
-    for (bsp_queue_t* qPtr = q; qPtr->data != NULL; qPtr = qPtr->next)
-    {
-        int bsp_axis = rand()%2;
-        int nrms = make_rms(qPtr->data, bsp_axis);
-
-        bsp_queue_t* tail;
-        for (tail = qPtr; tail->next != NULL; tail = tail->next);
-        tail->next = (bsp_queue_t*)calloc(1, sizeof(bsp_queue_t));
-
-        if (nrms == 2)
-        {
-            tail->next->data = qPtr->data->children[0];
-            tail->next->next = (bsp_queue_t*)calloc(1, sizeof(bsp_queue_t));
-            tail->next->next->data = qPtr->data->children[1];
-        }
-        else if (nrms == 1)
-        {
-            if (qPtr->data->children[0] != NULL)
-                tail->next->data = qPtr->data->children[0];
-            else
-                tail->next->data = qPtr->data->children[1];
-        }
-        else
-            break;
-#ifdef _DEBUG
-    bsp_node_t* p = NULL;
-    if ((p = qPtr->data->children[0]) != NULL)
-        printf("RM1: x: %d, y: %d, w: %d, h: %d\n", p->r.x, p->r.y, p->r.w, p->r.h);
-    if ((p = qPtr->data->children[1]) != NULL)
-        printf("RM2: x: %d, y: %d, w: %d, h: %d\n", p->r.x, p->r.y, p->r.w, p->r.h);
-#endif
-    }
+    char buf[256]; // msg data;
+    strcpy(buf, "MSG BUFFER IS NOT INITALIZED");
+    pthread_t th;
+    as_arg_t as_arg = {renderer, &logr, logr_startx,
+                      logr_starty ,texture_cache, buf};
+    pthread_create(&th, NULL, poll_serv, &as_arg);
 
     tiles_e map[MAP_WIDTH * MAP_HEIGHT];
     for (int i=0; i < MAP_WIDTH * MAP_HEIGHT; i++)
         map[i] = FLOOR;
-    for (bsp_queue_t* qPtr = q; qPtr->data != NULL; qPtr = qPtr->next)
+
+    for (int y=0; y < MAP_HEIGHT; y++)
     {
-        SDL_Rect rm = qPtr->data->r;
-        for (int y=0; y < MAP_HEIGHT; y++)
+        for (int x=0; x < MAP_WIDTH; x++)
         {
-            for (int x=0; x < MAP_WIDTH; x++)
-            {
-                // top
-                if (y == rm.y)
-                   map[(y * MAP_WIDTH) + x] = WALL;
-                // right
-                if (y >= rm.y && y < rm.y + rm.h && x == (rm.x + rm.w) - 1)
-                    map[(y * MAP_WIDTH) + x] = WALL;
-                // bottom
-                if (y == (rm.y + rm.h) - 1)
-                    map[(y * MAP_WIDTH) + x] = WALL;
-                // left
-                if (y >= rm.y && y < rm.y + rm.h && x == rm.x)
-                    map[(y * MAP_WIDTH) + x] = WALL;
-            }
+            // top
+            if (y == 0)
+               map[(y * MAP_WIDTH) + x] = WALL;
+            // right
+            if (x == MAP_WIDTH - 1)
+                map[(y * MAP_WIDTH) + x] = WALL;
+            // bottom
+            if (y == MAP_HEIGHT - 1)
+                map[(y * MAP_WIDTH) + x] = WALL;
+            // left
+            if (x == 0)
+                map[(y * MAP_WIDTH) + x] = WALL;
+
         }
     }
 
@@ -290,37 +244,6 @@ int main()
                    break;
                case SDLK_q:
                    return 0;
-               case SDLK_l:
-               {
-                    logr.y += logr.h;//*2;
-                    for (char* msgPtr = buf; *msgPtr != 0; msgPtr++)
-                    {
-                        if (*msgPtr == '\n')
-                            *msgPtr = ' ';
-
-                        if (logr.x >= SCREEN_WIDTH-(logr.w*2))
-                        {
-                            logr.x = logr_startx;
-                            logr.y += logr.h;
-                        }
-
-                        SDL_RenderCopy(
-                                renderer,
-                                texture_cache[(int)*msgPtr-ASCII_OFFSET],
-                                NULL,   // srcrect
-                                &logr
-                        );
-                        logr.x += logr.w;
-                    }
-                    logr.x = logr_startx;
-
-                    if (logr.y >= SCREEN_HEIGHT-(logr.h*2))
-                    {
-                        logr.y = logr_starty;
-                        SDL_RenderClear(renderer);
-                    }
-               }
-               break;
             } // switch
 
             for (int y=0; y < MAP_HEIGHT; y++)
@@ -339,63 +262,7 @@ int main()
 
         } // if SDL_KEYDOWN
     }} //  poll event & while(1)
-
     return 0;
-}
-
-void init_bsp_node(bsp_node_t* node, bsp_node_t* parent,
-    int w, int h, int x, int y)
-{
-    node->r.w = w;
-    node->r.h = h;
-    node->r.x = x;
-    node->r.y = y;
-    node->parent = parent;
-    node->connected = 0;
-}
-
-int make_rms(bsp_node_t* root, int axis)
-{
-    int nrms = 0;
-
-    float range = (rand() % (MAX_RM_SIZE - MIN_RM_SIZE)) / 10.0f;
-    int sizey = root->r.h * range;
-    int sizex = root->r.w * range;
-    int part1_sz = (axis) ? sizey : sizex;
-    int part2_sz = (axis) ? root->r.h - sizey : root->r.w - sizex;
-
-    root->children[0] = NULL;
-    root->children[1] = NULL;
-
-    if (part1_sz >= MIN_RM_SIZE)
-    {
-        root->children[0] = (bsp_node_t*)calloc(1, sizeof(bsp_node_t));
-        int w = (axis) ? root->r.w : part1_sz;
-        int h = (axis) ? part1_sz : root->r.h;
-        int x = root->r.x;
-        int y = root->r.y;
-        init_bsp_node(root->children[0], root, w, h, x, y);
-        nrms++;
-    }
-    if (part2_sz >= MIN_RM_SIZE)
-    {
-        root->children[1] = (bsp_node_t*)calloc(1, sizeof(bsp_node_t));
-        int w = (axis) ? root->r.w : part2_sz;
-        int h = (axis) ? part2_sz : root->r.h;
-        int x = root->r.x;
-        int y = root->r.y;
-        if (root->children[0] != NULL)
-        {
-            if (axis)
-                y+=h;
-            else
-             x+=w;
-        }
-        init_bsp_node(root->children[1], root, w, h, x, y);
-        nrms++;
-    }
-
-    return nrms;
 }
 
 int get_conn(void)
@@ -443,7 +310,7 @@ int get_conn(void)
     return sockfd;
 }
 
-void* poll_serv(void* arg)
+void* poll_serv(void* as_arg)
 {
     int sockfd = get_conn();
     if (sockfd == -1)
@@ -458,7 +325,7 @@ void* poll_serv(void* arg)
     int fd_count = 0;
     int fd_size = 1;
     fd_count = 1;
-    char* buf = (char*)arg;
+    char* msg = (char*)((as_arg_t*)as_arg)->msg;
     while(1)
     {
         int poll_count = poll(&pfd, fd_count, -1);
@@ -469,7 +336,7 @@ void* poll_serv(void* arg)
         }
         if (pfd.revents & POLLIN)
         {
-            int nbytes = recv(pfd.fd, buf, 255, 0);
+            int nbytes = recv(pfd.fd, msg, 255, 0);
             if (nbytes <= 0)
             {
                 if (nbytes == 0)
@@ -484,8 +351,46 @@ void* poll_serv(void* arg)
                 exit(1);
             }
             else
-                buf[nbytes] = 0;
+            {
+                msg[nbytes] = 0;
+                tf_log_handler(as_arg);
+            }
         }
     }
     pthread_exit(0);
+}
+
+void* tf_log_handler(void* as_arg)
+{
+    as_arg_t *arg = (as_arg_t*)as_arg;
+    for (char* msgPtr = arg->msg; *msgPtr != 0; msgPtr++)
+    {
+        if (*msgPtr == '\n')
+            *msgPtr = ' ';
+
+        if (arg->logr->x >= SCREEN_WIDTH - arg->logr->w)
+        {
+            arg->logr->x = arg->logr_startx;
+            arg->logr->y += arg->logr->h;
+        }
+
+        SDL_Delay(LOG_SPEED);
+        SDL_RenderCopy(
+                arg->renderer,
+                arg->texture_cache[(int)*msgPtr-ASCII_OFFSET],
+                NULL,   // srcrect
+                arg->logr
+        );
+        SDL_RenderPresent(arg->renderer);
+        arg->logr->x += arg->logr->w;
+    }
+    arg->logr->y += arg->logr->h;
+    arg->logr->x = arg->logr_startx;
+
+    if (arg->logr->y >= SCREEN_HEIGHT - arg->logr->h)
+    {
+        arg->logr->y = arg->logr_starty;
+        SDL_RenderClear(arg->renderer); //TODO: this wipes game view as
+                                        // well, update only logr bit...
+    }
 }
